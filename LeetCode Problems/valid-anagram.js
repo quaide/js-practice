64572e02512b9d663fe9d6551696bd2d0b107221
quaide <tranterq@gmail.com>
2024-01-31 15:11:48 -0500

    LC

--- LeetCode Problems/valid-anagram.js
+++ LeetCode Problems/valid-anagram.js
@@ -9,3 +9,23 @@ var isAnagram = function(s, t) {
     const stringTwo = t.split('').sort().toString();
     return stringOne === stringTwo;
 }
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    var freq = new Array(26).fill(0);
+    for (var i = 0; i < s.length; i++) {
+        freq[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        freq[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+    
+    for (var i = 0; i < freq.length; i++) {
+        if (freq[i] !== 0) {
+            return false;
+        }
+    }
+    
+    return true;
+};
